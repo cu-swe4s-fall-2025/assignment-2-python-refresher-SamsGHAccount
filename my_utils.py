106f76bc9682e5badfa33af91cb5bb1a3ef76914
Silver <adde2205@colorado.edu>
2025-09-07 20:26:38 -0600

    my_utils get_column() function properly operating, print_fires.py now modified to return US fire values

--- my_utils.py
+++ my_utils.py
@@ -1,2 +1,11 @@
 def get_column(file_name, query_column, query_value, result_column):
-    return None
+    f = open(file_name, 'r')
+
+    toReturn = []
+
+    for l in f:
+        A = l.rstrip().split(',')
+        if(A[query_column] == query_value):
+            toReturn.append(A[result_column])
+
+    return toReturn
