2a81b11168f5677dbf563ea4bac5a4cce9c2c90b
Silver <adde2205@colorado.edu>
2025-09-09 18:37:13 -0600

    Modified README.md to better explain the functions AND how to start quickly. Run.sh slightly corrected for technical correctness.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 
-echo "Running print_fires.py such that the fires of the United States are returned."
+echo "Running print_fires.py such that the quantity of CO2 released from fires in the United States are returned."
 python print_fires.py
 echo "Finished running print_fires.py"
