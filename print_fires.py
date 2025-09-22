7c91dbf5b2d33337898b999788a655d5828b8f3c
Silver <adde2205@colorado.edu>
2025-09-21 22:00:53 -0600

    The modified print_fires.py which should now return custom errors and allow for custom inputs.

--- print_fires.py
+++ print_fires.py
@@ -1,8 +1,29 @@
+import argparse
 import my_utils as mu
+def main():
+    parser = argparse.ArgumentParser(description="Returns a column of data for specific country.")
+    parser.add_argument('--file', type=str, required=True, help='CSV file name')
+    parser.add_argument('--country', type=str, required=True, help='Country to search')
+    parser.add_argument('--county_column', type=int, default=0, help='Index for country name')
+    parser.add_argument('--return_col', type=int, required=True, help='Index to return')
 
-country='United States of America'
-county_column = 0
-fires_column = 3
-file_name = 'Agrofood_co2_emission.csv'
-fires = mu.get_column(file_name, county_column, country, fires_column)
-print(fires)
+    args = parser.parse_args()
+
+    try:
+        return_raw = mu.get_column(args.file, args.county_column, args.country, args.return_col)
+
+        return_int = [int(float(x)) for x in return_raw]
+
+        print(return_int)
+
+    except FileNotFoundError:
+        print("Could not find file " + args.file)
+    except PermissionError:
+        print("Not allowed to access " + args.file)
+    except ValueError:
+        print("Could not convert values to integers!")
+    except Exception:
+        print("Uh-oh! Something unexpected happened!")
+
+if __name__ == "__main__":
+    main()
