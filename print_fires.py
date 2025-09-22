19e15f9e9c1b634df5f592829a1deceae074e111
Silver <adde2205@colorado.edu>
2025-09-21 22:05:35 -0600

    Added comments

--- print_fires.py
+++ print_fires.py
@@ -1,5 +1,7 @@
 import argparse
 import my_utils as mu
+
+#Adds the different arguments, if they're required, and defaults.
 def main():
     parser = argparse.ArgumentParser(description="Returns a column of data for specific country.")
     parser.add_argument('--file', type=str, required=True, help='CSV file name')
@@ -16,12 +18,16 @@ def main():
 
         print(return_int)
 
+    #returns an error when the file isn't found
     except FileNotFoundError:
         print("Could not find file " + args.file)
+    #returns an error when the program isn't allowed to access the file
     except PermissionError:
         print("Not allowed to access " + args.file)
+    #returns an error when the program can't convert the values to integers
     except ValueError:
         print("Could not convert values to integers!")
+    #returns a broad error
     except Exception:
         print("Uh-oh! Something unexpected happened!")
 
