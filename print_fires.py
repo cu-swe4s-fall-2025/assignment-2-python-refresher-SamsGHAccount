8160bf0e56152e5f0f6ed0a2760f24c18040bab2
Silver <adde2205@colorado.edu>
2025-09-29 11:23:16 -0600

    Modified print_fires.py to now return the mean, median, or standard deviation upon request.

--- print_fires.py
+++ print_fires.py
@@ -8,6 +8,7 @@ def main():
     parser.add_argument('--country', type=str, required=True, help='Country to search')
     parser.add_argument('--county_column', type=int, default=0, help='Index for country name')
     parser.add_argument('--return_col', type=int, required=True, help='Index to return')
+    parser.add_argument('--info_ret', type=str, required=False, help='Info to return')
 
     args = parser.parse_args()
 
@@ -15,8 +16,22 @@ def main():
         return_raw = mu.get_column(args.file, args.county_column, args.country, args.return_col)
 
         return_int = [int(float(x)) for x in return_raw]
-
-        print(return_int)
+        if(args.info_ret=='mean'):
+            print(sum(return_int)/len(return_int))
+        elif(args.info_ret=='median'):
+            return_int.sort()
+            if(len(return_int)%2==0):
+                median = (return_int[len(return_int)//2] + return_int[len(return_int)//2 - 1]) / 2
+            else:
+                median = return_int[len(return_int)//2]
+            print(median)
+        elif(args.info_ret=='stdev'):
+            mean = sum(return_int)/len(return_int)
+            variance = sum((x - mean) ** 2 for x in return_int) / len(return_int)
+            stdev = variance ** 0.5
+            print(stdev)
+        else:
+            print(return_int)
 
     #returns an error when the file isn't found
     except FileNotFoundError:
